c437755fb8efb7070c2c1b34f2af9182a3be140c
AwrenNuit <a.rennaker@yahoo.com>
2019-12-29 15:47:51 -0600

    functional product

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require(`express`);
+require(`dotenv`).config();
+const app = express();
+const bodyParser = require(`body-parser`);
+const PORT = process.env.PORT || 5000;
+// const imageRouter = require(`./routes/imageRouter`);
+
+const UploaderS3Router = require('react-dropzone-s3-uploader/s3router');
+
+app.use(express.static(`server/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+// app.use(`/image`, imageRouter);
+
+app.use('/s3', UploaderS3Router({
+  bucket: 'prime-solo-test',                           // required
+  region: 'us-east-2',                            // optional
+  headers: {'Access-Control-Allow-Origin': '*'},  // optional
+  ACL: 'public-read',                                 // this is the default - set to `public-read` to let anyone view uploads
+}));
+
+app.listen(PORT, ()=>{
+    console.log('server up on', PORT);
+});
---
+++ src/ImageUpload.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import DropzoneS3Uploader from 'react-dropzone-s3-uploader';
+
+const dropzoneStyle = {
+    border: '1px solid black',
+    height: '50px',
+    width: '200px',
+    boxShadow: '0 0 30px white',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    cursor: 'pointer'
+}
+
+class ImageUpload extends Component{
+
+  render(){
+    const uploadOptions = {
+      server: 'http://localhost:5000'
+    }
+
+    const s3Url = 'https://prime-solo-test.s3.amazonaws.com';
+
+    const innerEl = (
+      <div className='zone'>
+        <p>Click to upload</p>
+      </div>
+    )
+
+    return (
+      <DropzoneS3Uploader
+        onFinish={this.handleFinishedUpload}
+        style={dropzoneStyle}
+        s3Url={s3Url}
+        maxSize={1024 * 1024 * 5}
+        upload={uploadOptions}
+        children={innerEl}
+      />
+    )
+  }
+}
+
+export default connect()(ImageUpload);
